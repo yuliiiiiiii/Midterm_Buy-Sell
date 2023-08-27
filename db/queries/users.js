c196ea188fc1eeaeaab4fab7c67540147366e563
caro-turnbull <caro.turnbull@gmail.com>
2023-08-27 10:09:17 -0400

    atempt at seeds

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
+  return db.query('SELECT * FROM user;')
     .then(data => {
       return data.rows;
     });

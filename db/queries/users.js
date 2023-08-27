200d0fffa73709260ae331d844fb4401411c3a78
caro-turnbull <caro.turnbull@gmail.com>
2023-08-27 13:43:57 -0400

    seeds and schema updated to artist, started query and routes

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,12 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+const getUserById = () => {
+  return db.query('SELECT * FROM user WHERE id=1;') //this will need to be made dynamic
+    .then(data => {
+      console.log("user by id data", data)
+      return data
+    })
+}
+
+module.exports = { getUsers, getUserById };
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO user (id, name, email, password)
+INSERT INTO artist (id, name, email, password)
   VALUES
-    (1, 'Vincent van Gogh', 'vinny@example.com', 'password')
-    (2, 'Edgar Degas', 'edgar@example.com', 'password')
-    (3, 'Katsushika Hokusai', 'hokusai@example.com', 'password')
-    (4, 'Hans Baldung', 'hansB@example.com', 'password')
+    (1, 'Vincent van Gogh', 'vinny@example.com', 'password'),
+    (2, 'Edgar Degas', 'edgar@example.com', 'password'),
+    (3, 'Katsushika Hokusai', 'hokusai@example.com', 'password'),
+    (4, 'Hans Baldung', 'hansB@example.com', 'password'),
     (5, 'Paul Cézanne', 'paul@example.com', 'password');

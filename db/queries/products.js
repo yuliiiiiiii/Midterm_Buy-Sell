2b72ac499e30fde32b9cd73596b1ef8f224d40d0
caro-turnbull <caro.turnbull@gmail.com>
2023-08-28 14:56:18 -0400

    working on profile page multiple queries

--- db/queries/products.js
+++ db/queries/products.js
@@ -1,9 +1,11 @@
 const db = require('../connection');
 
 const getProductsbySeller = () => {
-  return db.query(`SELECT *
-                    FROM product
-                    WHERE seller_id = 3;`)
+  return db.query(`SELECT *, product.name as product_name
+                  FROM product
+                  JOIN artist
+                  ON product.seller_id = artist.id
+                  WHERE seller_id = 3;`)
     .then(data => {
       return data.rows;
     });
--- routes/profile.js
+++ routes/profile.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
-const userQueries = require('../db/queries/users');
+// const userQueries = require('../db/queries/users');
+const productQueries = require('../db/queries/products');
 
 router.get('/', (req, res) => {
 
-  userQueries.getUserById() //this will need to be made dynamic, here or in query??
+
+  // userQueries.getUserById() //this will need to be made dynamic, here or in query??
+  //   .then(data => {
+  //     const userProfile = data.rows[0];
+  //     console.log("----data", userProfile)
+  //     // const templateVars = { userProfile }
+  //     // return res.render('profile', templateVars)
+  //     return userProfile
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+
+  productQueries.getProductsbySeller() //dynamic
     .then(data => {
-      const userProfile = data.rows[0]; //rows?
-      console.log("----data", userProfile)
-      // res.json({ userProfile });  //do i need to json the sata for one user's info
-      const templateVars = { userProfile }
+      const sellersProducts = data;
+      console.log("--------product data", sellersProducts)
+      const templateVars = {sellersProducts}
       return res.render('profile', templateVars)
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+  })
+
 });
 
 module.exports = router;

a9d1929558999eb143ccd8c2f317f91b8a0b6985
caro-turnbull <caro.turnbull@gmail.com>
2023-08-29 17:20:36 -0400

    updated user seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO artist (id, name, email, password)
+INSERT INTO artist (name, email, password)
   VALUES
-    (1, 'Vincent van Gogh', 'vinny@example.com', 'password'),
-    (2, 'Edgar Degas', 'edgar@example.com', 'password'),
-    (3, 'Katsushika Hokusai', 'hokusai@example.com', 'password'),
-    (4, 'Hans Baldung', 'hansB@example.com', 'password'),
-    (5, 'Paul Cézanne', 'paul@example.com', 'password'),
-    (6, 'Georgia 0Keefe', 'flowers@example.com', 'password');
+    ('Vincent van Gogh', 'vinny@example.com', 'password'),
+    ('Edgar Degas', 'edgar@example.com', 'password'),
+    ('Katsushika Hokusai', 'hokusai@example.com', 'password'),
+    ('Hans Baldung', 'hansB@example.com', 'password'),
+    ('Paul Cézanne', 'paul@example.com', 'password'),
+    ('Georgia 0Keefe', 'flowers@example.com', 'password');

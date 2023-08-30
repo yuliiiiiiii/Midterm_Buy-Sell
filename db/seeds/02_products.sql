cac455c4be352df57bf8c17f65231d23e2587563
caro-turnbull <caro.turnbull@gmail.com>
2023-08-30 16:06:08 -0400

    updated seed to include one sold

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,30 +1,30 @@
-INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents)
+INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents, sold)
   VALUES
-    (1, 'Self-Portrait with a Straw Hat', 'Oil on canvas','https://images.metmuseum.org/CRDImages/ep/original/DT1502_cropped2.jpg', 3000),
-    (1, 'Wheat Field with Cypresses', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg', 2000),
-    (2, 'The Dance Class', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DP-20101-001.jpg', 4500),
-    (2, 'The Little Fourteen-Year-Old Dancer', 'Partially tinted bronze, cotton tarlatan, silk satin, and wood', 'https://images.metmuseum.org/CRDImages/es/original/DP-14939-002.jpg', 500),
-    (3, 'Under the Wave off Kanagawa', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141042.jpg', 3900),
-    (3, 'The Lake at Hakone in Sagami Province', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141075.jpg', 3800),
-    (4, 'Portrait of Charles V','Woodcut and letterpress text','https://images.metmuseum.org/CRDImages/dp/original/DP826741.jpg', 1500 ),
-    (4, 'Group of Seven Horses', 'Woodcut', 'https://images.metmuseum.org/CRDImages/dp/original/DP826744.jpg', 2200),
-    (5, 'Still Life with Apples and a Pot of Primroses', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DT47.jpg', 2000),
-    (5, 'Trees and Houses Near the Jas de Bouffan', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/rl/original/DT3108.jpg', 1200),
-    (3, 'The Suspension Bridge on the Border of Hida and Etchū Provinces', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141245.jpg', 3700),
-    (3, 'Kirifuri Waterfall at Kurokami Mountain in Shimotsuke', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141247.jpg', 3600),
-    (1, 'Starry Night', 'oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1513px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg', 8000);
+    (1, 'Self-Portrait with a Straw Hat', 'Oil on canvas','https://images.metmuseum.org/CRDImages/ep/original/DT1502_cropped2.jpg', 3000, false),
+    (1, 'Wheat Field with Cypresses', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg', 2000, false),
+    (2, 'The Dance Class', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DP-20101-001.jpg', 4500, false),
+    (2, 'The Little Fourteen-Year-Old Dancer', 'Partially tinted bronze, cotton tarlatan, silk satin, and wood', 'https://images.metmuseum.org/CRDImages/es/original/DP-14939-002.jpg', 500, false),
+    (3, 'Under the Wave off Kanagawa', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141042.jpg', 3900, false),
+    (3, 'The Lake at Hakone in Sagami Province', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141075.jpg', 3800, true),
+    (4, 'Portrait of Charles V','Woodcut and letterpress text','https://images.metmuseum.org/CRDImages/dp/original/DP826741.jpg', 1500 , false),
+    (4, 'Group of Seven Horses', 'Woodcut', 'https://images.metmuseum.org/CRDImages/dp/original/DP826744.jpg', 2200, false),
+    (5, 'Still Life with Apples and a Pot of Primroses', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/ep/original/DT47.jpg', 2000, false),
+    (5, 'Trees and Houses Near the Jas de Bouffan', 'Oil on canvas', 'https://images.metmuseum.org/CRDImages/rl/original/DT3108.jpg', 1200, false),
+    (3, 'The Suspension Bridge on the Border of Hida and Etchū Provinces', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141245.jpg', 3700, false),
+    (3, 'Kirifuri Waterfall at Kurokami Mountain in Shimotsuke', 'Woodblock print; ink and color on paper', 'https://images.metmuseum.org/CRDImages/as/original/DP141247.jpg', 3600, false),
+    (1, 'Starry Night', 'oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1513px-Van_Gogh_-_Starry_Night_-_Google_Arfalset_Project.jpg', 8000, false);
 
-INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents)
+INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents, sold)
   VALUES
-    (1, 'Cafe Terrace at Night', 'Oil on canvas', 'https://www.vangoghstudio.com//Files/6/102000/102147/ProductPhotos/Source/1938150426.jpg', 3500),
-    (2, 'The Dance Foyer at the Opera', 'Oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/5/52/Edgar_Degas_-_The_Dance_Foyer_at_the_Opera_on_the_rue_Le_Peletier.jpg', 4200),
-    (5, 'Mont Sainte-Victoire', 'Oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/0/00/Paul_C%C3%A9zanne_-_Montagne_Saint-victoire_-_Google_Art_Project.jpg', 3500),
-    (6, 'Blue and Green Music', 'Oil on canvas', 'https://www.artic.edu/iiif/2/3ee54063-9d78-ee86-0103-b477d988a93f/full/843,/0/default.jpg', 2700),
-    (6, 'Red Canna', 'Oil on canvas', 'https://arthive.net/res/media/img/oy800/work/98f/439600@2x.jpg', 2300),
-    (1, 'Sunflowers', 'Oil on canvas', 'https://www.thenationalnews.com/resizer/oUWnEcIahNVsCxifmpdHeI-2xtQ=/arc-photo-thenational/eu-central-1-prod/public/67UXZMBVWSOSLEW7SJ72I62NS4.jpg', 4600);
+    (1, 'Cafe Terrace at Night', 'Oil on canvas', 'https://www.vangoghstudio.com//Files/6/102000/102147/ProductPhotos/Source/1938150426.jpg', 3500, false),
+    (2, 'The Dance Foyer at the Opera', 'Oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/5/52/Edgar_Degas_-_The_Dance_Foyer_at_the_Opera_on_the_rue_Le_Peletier.jpg', 4200, false),
+    (5, 'Mont Sainte-Victoire', 'Oil on canvas', 'https://upload.wikimedia.org/wikipedia/commons/0/00/Paul_C%C3%A9zanne_-_Montagne_Saint-victoire_-_Google_Art_Project.jpg', 3500, false),
+    (6, 'Blue and Green Music', 'Oil on canvas', 'https://www.artic.edu/iiif/2/3ee54063-9d78-ee86-0103-b477d988a93f/full/843,/0/default.jpg', 2700, false),
+    (6, 'Red Canna', 'Oil on canvas', 'https://arthive.net/res/media/img/oy800/work/98f/439600@2x.jpg', 2300, false),
+    (1, 'Sunflowers', 'Oil on canvas', 'https://www.thenationalnews.com/resizer/oUWnEcIahNVsCxifmpdHeI-2xtQ=/arc-photo-thenational/eu-central-1-prod/public/67UXZMBVWSOSLEW7SJ72I62NS4.jpg', 4600, false);
 
-INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents)
+INSERT INTO product (seller_id, name, description, link_to_pic, price_in_cents, sold)
   VALUES
-    (7, 'The Thinker', 'Bronze sculpture', 'https://images.metmuseum.org/CRDImages/es/original/DP-13618-012.jpg', 5000),
-    (7, 'The Cathedral', 'Stone Sculpture', 'https://www.musee-rodin.fr/sites/default/files/styles/diaporama_mav/public/2020-12/s.1001_web_numjm001.jpg?itok=UmrtZo57', 1900),
-    (7, 'Crying Lion', 'Bronze sculpture', 'https://www.christies.com/img/LotImages/2013/PAR/2013_PAR_03567_0014_000(auguste_rodin_le_lion_qui_pleure085606).jpg', 4800);
+    (7, 'The Thinker', 'Bronze sculpture', 'https://images.metmuseum.org/CRDImages/es/original/DP-13618-012.jpg', 5000, false),
+    (7, 'The Cathedral', 'Stone Sculpture', 'https://www.musee-rodin.fr/sites/default/files/styles/diaporama_mav/public/2020-12/s.1001_web_numjm001.jpg?itok=UmrtZo57', 1900, false),
+    (7, 'Crying Lion', 'Bronze sculpture', 'https://www.christies.com/img/LotImages/2013/PAR/2013_PAR_03567_0014_000(auguste_rodin_le_lion_qui_pleure085606).jpg', 4800, false);
